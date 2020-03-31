34134cc2fd66801d606c33750afc27bb1a0cfd2d
Mona Lisa <kbell28k@gmail.com>
2020-03-31 12:45:58 -0600

    re-configed index page and sign in / signup

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,27 +8,34 @@ var path = require("path");
 
 // Routes
 // =============================================================
-module.exports = function(app) {
-
+module.exports = function (app) {
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
   // index route loads view.html
-  app.get("/",  function(req, res) {
-    res.render("index",{})
+  app.get("/", function (req, res) {
+    res.render("index", {});
   });
 
   // cms route loads cms.html
-  app.get("/seller", function(req, res) {
-    res.render("seller",{})
+  app.get("/seller", function (req, res) {
+    res.render("seller", {});
+  });
+
+  app.get("/signup", function (req, res) {
+    res.render("signup", {});
+  });
+
+  app.get("/login", function (req, res) {
+    res.render("login", {});
   });
 
   // blog route loads blog.html
-  app.get("/blog", function(req, res) {
+  app.get("/blog", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/blog.html"));
   });
 
   // authors route loads author-manager.html
-  app.get("/authors", function(req, res) {
+  app.get("/authors", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/author-manager.html"));
   });
 
@@ -36,5 +43,4 @@ module.exports = function(app) {
   app.get("/img-upload", (req, res) => {
     res.sendFile(path.join(__dirname, "img_upload.html"));
   });
-
 };
