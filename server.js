3b2311f923c017c170350084d1a19d471d3a57de
Mona Lisa <kbell28k@gmail.com>
2020-03-31 15:09:01 -0600

    changes to passport for working version

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express"),
   bodyParser = require("body-parser"),
   //env = require('dotenv').load(),
   exphbs = require("express-handlebars");
-  cloudinary = require('cloudinary').v2;
+cloudinary = require("cloudinary").v2;
 
 var PORT = process.env.PORT || 8080;
 
@@ -27,15 +27,22 @@ app.use(passport.session());
 
 // Cloudinary Config
 
-cloudinary.config({ 
-  cloud_name: 'dhitmyxio', 
-  api_key: '434933154362283', 
-  api_secret: '6t_Ajztu1CGhNdF4GswNfotXduw' 
+cloudinary.config({
+  cloud_name: "dhitmyxio",
+  api_key: "434933154362283",
+  api_secret: "6t_Ajztu1CGhNdF4GswNfotXduw"
 });
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
+// We need to use sessions to keep track of our user's login status
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
@@ -43,6 +50,7 @@ app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 require("./routes/html-routes.js")(app);
+require("./routes/api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
