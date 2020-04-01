8a3308975ef9415ca6db09c58f8a774252ace13f
Mona Lisa <kbell28k@gmail.com>
2020-03-31 19:22:20 -0600

    passport changes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,9 +14,11 @@ module.exports = function (app) {
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
   // otherwise send back an error
   app.post("/api/signup", function (req, res) {
+    console.log({ t: "signup", body: req.body });
     db.User.create({
       email: req.body.email,
-      password: req.body.password
+      password: req.body.password,
+      zipCode: req.body.zipcode
     })
       .then(function () {
         res.redirect(307, "/api/login");
