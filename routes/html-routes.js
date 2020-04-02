012ec50be58b8dadfc6245c764252f582cf7b04a
Mona Lisa <kbell28k@gmail.com>
2020-04-02 09:57:03 -0600

    heroku testing

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -13,6 +13,9 @@ var isAuthenticated = require("../config/middleware/isAuthenticated");
 // Routes
 // =============================================================
 module.exports = function (app) {
+  app.get("/", function (req, res) {
+    res.render("index", {});
+  });
   // Each of the below routes just handles the HTML page that the user gets sent to.
   app.get("/", function (req, res) {
     // If the user already has an account send them to the members page
@@ -43,25 +46,23 @@ module.exports = function (app) {
     console.log("reached members page");
     if (!req.user) {
       res.redirect("/");
-      return
+      return;
     }
     var query = {};
-   const zipCode = req.user.zipCode;
-   query.item_zipCode =zipCode ;
-   const data = await  db.Post.findAll({
+    const zipCode = req.user.zipCode;
+    query.item_zipCode = zipCode;
+    const data = await db.Post.findAll({
       where: query,
       include: [db.User]
-    }).map(el => el.get({ plain: true })) ;
+    }).map(el => el.get({ plain: true }));
 
-    console.log(data   )
-     res.render("allitems", { dbPost: data    , layout: 'main'});
-   
-    
+    console.log(data);
+    res.render("allitems", { dbPost: data, layout: "main" });
   });
 
   // cms route loads cms.html
   app.get("/seller", function (req, res) {
-    res.render("seller", {title: 'my other page', layout: 'main'});
+    res.render("seller", { title: "my other page", layout: "main" });
   });
 
   app.get("/signup", function (req, res) {
